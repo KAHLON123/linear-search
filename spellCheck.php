4a8ad6ff0f26e9a90af15b213aa3b620b44d6631
KAHLON123 <h.kahlon5@share.epsb.ca>
2023-03-09 11:22:44 -0700

    few syntax error, output index instead of array value

---
+++ spellCheck.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>Spell Check</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+  <h1>Spell Check Assignment</h1>
+  <hr>
+
+  <h2>Single Word Spell Check</h2>
+  <p>Enter a Word: <input type="text" id="word"></p>
+<section>
+    <form>
+        <select name="selection">
+            <option></option>
+
+        </select>
+    </form>
+</section>
+  <p>Word Results: <span id="word-results">-----</span></p>
+  <hr>
+
+  <h2>Alice in Wonderland Spell Check</h2>
+  <p>
+    <button id="alice-linear-btn">Check Alice (Linear)</button>
+    <button id="alice-binary-btn">Check Alice (Binary)</button>
+  </p>
+  <p>Alice Results: <span id="alice-results">-----</span></p>
+
+</body>
+
+</html>
+
+<?php
+if (isset($_GET['submit'])){
+    switch {
+        case
+        
+    }
+}
+?>
