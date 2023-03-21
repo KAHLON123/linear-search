99ee47912f32bf255491cb59718c0f61f1a39d54
KAHLON123 <h.kahlon5@share.epsb.ca>
2023-03-21 11:23:15 -0600

    Create sort.php

---
+++ sort.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Sort Algorithims</title>
+</head>
+<body>
+<?php
+$nums = [10, 70, 30, 100, 40, 45, 90, 80, 85];
+$words = ["dog","at", "good", "eye", "cat", "ball", "fish"];
+bubblesort($nums);
+bubblesort($words);
+function bubblesort($arr){
+    for ($n = 1; $n < count($arr) - 1; $n++){
+        foreach()
+    }
+var_dump($arr);
+}
+
+?>
+</body>
+</html>
