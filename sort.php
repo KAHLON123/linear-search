a63fb6bc105b642a2f528c4b9f682333d6fedc05
KAHLON123 <h.kahlon5@share.epsb.ca>
2023-03-23 11:23:06 -0600

    Update sort.php

--- sort.php
+++ sort.php
@@ -4,18 +4,45 @@
     <title>Sort Algorithims</title>
 </head>
 <body>
+
+
+<h1>bubblesorddd</h1>
 <?php
-$nums = [10, 70, 30, 100, 40, 45, 90, 80, 85];
+function printArr($arr){
+    for ($i = 0; $i < count($arr); $i++) {
+        echo $arr[$i], "<br/>";
+    }
+}
+
+$nums = [9, 8, 7, 6, 5, 4, 3, 2, 1];
+$selectionNums = [6, 9, 2, 3, 5];
 $words = ["dog","at", "good", "eye", "cat", "ball", "fish"];
 bubblesort($nums);
 bubblesort($words);
-function bubblesort($arr){
-    for ($n = 1; $n < count($arr) - 1; $n++){
-        foreach()
+function bubblesort($arr){ // problem: only iterates for index 0...
+    for ($n = 0; $n < count($arr) - 1; $n++) {
+        if ($arr[$n] > $arr[$n + 1]) {
+            $hold = $arr[$n];
+            $arr[$n] = $arr[$n + 1];
+            $arr[$n + 1] = $hold;
+        }
+    
     }
-var_dump($arr);
+    printArr($arr);
 }
+?>
 
+
+<h1>selection srod</h1>
+<?php
+function selectionSort($arr){
+    for ($n = 0; $n < count($arr) - 1; $n++) {
+        $fill = $arr[$n];
+        for ($n = 1; $n < count($arr) - 1; $n++) {
+            if ()
+        }
+    }
+}
 ?>
 </body>
 </html>
